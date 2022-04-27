c02d76b30670520776c2661055609e9d420c4593
Samuel Peter <tellsammie@gmail.com>
2022-04-26 20:40:20 -0400

    remove main.py as not needed

--- examples/main.py
+++
@@ -1,9 +0,0 @@
-from initialization import RefineryCalc
-
-if __name__ == "__main__":
-    refs = [7, 8, 9, 10]
-    client = RefineryCalc(refs)
-    simulation_id = client.create_simulation("trend simulation")
-    if simulation_id is not None:
-        client.run_simulation(simulation_id)
-        client.rename_simulation("Justman")
