8115dbcee8cdf75afe3859d34769084357ec56b4
Samuel Peter <tellsammie@gmail.com>
2022-04-26 20:19:34 -0400

    Modified Create Simulation Example

--- examples/create_simulation.py
+++ examples/create_simulation.py
@@ -1,20 +1,17 @@
+from python.refinerycalc.models.create_simulation_request import CreateSimulationRequest
 from initialization import RefineryCalc
 
-
-def create_simulation(self, name: str):
-    req = create_simulation()
-    req.refineries = self.refineries
-    req.data_source = 0
-    req.name = name
-    req.is_time_series = False
-    response = self.api.v1_simulations_post(body=req)
-    if response.success:
-        return response.simulation_id
-    return None
-
 if __name__ == "__main__":
+    # you can get refinery ids with a separate call.
+    # hardcoded here for convenience
     refs = [7, 8, 9, 10]
-    client = RefineryCalc(refs)
-    simulation_id = client.create_simulation("sam simulation")
-    if simulation_id is not None:
-        client.run_simulation(simulation_id)
+    client = RefineryCalc()
+    req = CreateSimulationRequest()
+    req.refineries = refs
+    # 0 is default datasource and 1 for IIR
+    req.data_source = 0
+    req.name = "my new simulation"
+    req.is_time_series = False
+    response = client.api.v1_simulations_post(body=req)
+    if response.success:
+        print(response.simulation_id)
